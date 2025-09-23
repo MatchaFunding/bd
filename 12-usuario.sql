629feec12815254f9f6d362e3de827b4fa9a3055
ElMaikina <mikesoto890@gmail.com>
2025-09-23 18:23:28 -0300

    agregar campo de beneficiario al usuario para facilitar queries

--- 12-usuario.sql
+++ 12-usuario.sql
@@ -4,6 +4,7 @@ Clase que representa a un usuario de MatchaFunding.
 CREATE TABLE Usuario (
 	ID bigint NOT NULL AUTO_INCREMENT,
 	Persona bigint NULL,
+	Beneficiario bigint NULL,
 	NombreDeUsuario varchar(200),
 	Contrasena varchar(200) NOT NULL,
 	Correo varchar(200) NOT NULL,
@@ -16,6 +17,7 @@ CREATE VIEW VerTodosLosUsuarios AS SELECT
 	Usuario.NombreDeUsuario,
 	Usuario.Contrasena,
 	Usuario.Correo,
-	Usuario.Persona
+	Usuario.Persona,
+	Usuario.Beneficiario
 FROM
 	Usuario;
