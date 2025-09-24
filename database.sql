db67c02f821404658e289bc3c8640f7abc6dd748
ElMaikina <mikesoto890@gmail.com>
2025-09-24 19:33:31 -0300

    actualizar dump con vista estado de fondo

--- database.sql
+++ database.sql
@@ -385,6 +385,8 @@ VALUES
 	(6,"PAY","Patrocinio Institucional"),
 	(7,"DES","Desierto"),
 	(8,"CER","Cerrrado");
+
+CREATE VIEW VerEstadosDeFondo AS SELECT Nombre FROM EstadoDeFondo;
 /*
 Tipo de beneficio que otorga cierto fondo o instrumento.
 */
