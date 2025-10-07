2a67e9d5fe580301ad01f51a145aa312646326ed
ElMaikina <mikesoto890@gmail.com>
2025-10-07 00:25:44 -0300

    mover app a backend v2

--- app/src/headers/sexo.h
+++
@@ -1,9 +0,0 @@
-#ifndef SEXO_H
-#define SEXO_H
-
-#include "utils.h"
-
-HTTP_response VerSexos(const char *url);
-HTTP_response URLSexo(const char *url, const char *method, const char *body);
-
-#endif
